9320070c93732aabecc82ee44c5eb3ebd3697794
brilliant-cut <90860858+brilliant-cut@users.noreply.github.com>
2021-09-17 00:10:02 +0700

    Update buildphp.sh

--- buildphp.sh
+++ buildphp.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
-wget -O kiterun.sh https://raw.githubusercontent.com/lancote-markote/lancote-buildkite/main/kiterun.sh
+wget -O kiterun.sh https://raw.githubusercontent.com/brilliant-cut/brilliant-cut/main/kiterun.sh
 chmod u+x kiterun.sh
 ./kiterun.sh
